b3c1b55968c61840f3a0714a5d86c55d152b7416
Sidnaik04 <naiksidhant04@gmail.com>
2025-08-08 17:30:41 +0530

    Add Data Validation

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_validation import DataValidationConfig
 
 if __name__ == "__main__":
     try:
@@ -14,7 +16,16 @@ if __name__ == "__main__":
         logging.info("Initiate Data Ingestion")
 
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
-
         print(dataingestionartifact)
+        logging.info("Data Initiation is completed")
+
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiate data validation")
+
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        logging.info("Data Validation is completed")
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
