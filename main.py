45b91c72cbc7b26278c73cf4b6bf2021bc86a0e9
Sidnaik04 <naiksidhant04@gmail.com>
2025-08-08 19:07:59 +0530

    add data transformation

--- main.py
+++ main.py
@@ -6,26 +6,40 @@ from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_validation import DataValidationConfig
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.data_transformation import DataTransformationConfig
 
 if __name__ == "__main__":
     try:
+        # data ingestion
         trainingpipelineconfig = TrainingPipelineConfig()
-        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
 
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate Data Ingestion")
-
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
         logging.info("Data Initiation is completed")
 
+        # Data validaion
         data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Initiate data validation")
-
         data_validation_artifact = data_validation.initiate_data_validation()
         print(data_validation_artifact)
         logging.info("Data Validation is completed")
+
+        # data transformation
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Initiate data transformation")
+        data_transformation = DataTransformation(
+            data_validation_artifact, data_transformation_config
+        )
+        data_transformation_artifact = (
+            data_transformation.initiate_data_transformation()
+        )
+        print(data_transformation_artifact)
+        logging.info("Data transformation is completed")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
